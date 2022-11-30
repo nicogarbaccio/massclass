288e64b321e06b57a82748dfa18638b7576ce238
Nico Garbaccio <garbaccio20@gmail.com>
2022-11-30 11:57:06 -0500

    Course Details component

--- client/src/Components/CourseDetails.js
+++ client/src/Components/CourseDetails.js
@@ -1,9 +1,118 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useParams, NavLink, useNavigate } from 'react-router-dom';
+import { UserContext } from '../Context/user';
+import SyllabusForm from './SyllabusForm';
+import DeleteConfirmation from './DeleteConfirmation';
 
 function CourseDetails() {
+
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [course, setCourse] = useState([])
+    const [show, setShow] = useState(false)
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const { user } = useContext(UserContext);
+
+    useEffect(() => {
+        fetch(`/courses/${id}`)
+        .then((r) => r.json())
+        .then(course => {
+        setCourse(course);
+        setIsLoaded(true)
+    })
+    }, [id])
+
+    if (!isLoaded) return <h2>Loading...</h2>
+
+    function handleDeleteCourse() {
+
+        fetch(`/courses/${id}`, {
+            method:'DELETE'
+          })
+ 
+        navigate(`/instructor/${course.instructor.id}/courses`);
+        window.location.reload();
+    }
+
+    function handleToggle() {
+        setShow(!show)
+    }
+
     return (
         <div>
-            CourseDetails
+            <h1>{course.title} ({course.code})</h1>
+            <div>
+                {course.syllabus ?
+                    <p>
+                        <NavLink to={`/syllabus/${course.syllabus?.id}`}>
+                            <span>Syllabus</span>
+                        </NavLink>
+                    </p>
+                :
+                user.admin ?
+                    <div>
+                        <h2>Add a description for your course</h2>
+                        <SyllabusForm course={course} setCourse={setCourse} />
+                    </div>
+                :
+                <p>Your instructor hasn't posted a syllabus yet!</p>
+                }
+                <div>
+                    <button>
+                        <NavLink to={`/course/${id}/announcements`}>
+                            <span>Announcements</span>
+                        </NavLink>
+                    </button>
+                </div>
+                <div>
+                    <button>
+                        <NavLink to={`/course/${id}/assignments`}>
+                            <span>Assignments</span>
+                        </NavLink>
+                    </button>
+                </div>
+                <div>
+                    <button>
+                        <NavLink to={`/course/${id}/discussion_board`}>
+                            <span>Discussion Board</span>
+                        </NavLink>
+                    </button>
+                </div>
+                <div>
+                    <button>
+                        <NavLink to={`/course/${id}/documents`}>
+                            <span>Course Documents</span>
+                        </NavLink>
+                    </button>
+                </div>
+                {user?.admin ?
+                    <div>
+                        <button>
+                            <NavLink to={`/course/${id}/students`}>
+                                <span>Students</span>
+                            </NavLink>
+                        </button>
+                    </div>
+                :
+                    <div>
+                        <button>
+                            <NavLink to={`/course/${id}/grades`}>
+                                <span>Grades</span>
+                            </NavLink>
+                        </button>
+                    </div>
+                }
+            </div>
+            <div>
+                {user?.admin ?
+                    <button onClick={handleToggle}>Delete Course</button>
+                :
+                ""
+                }
+            </div>
+            <div>
+                <DeleteConfirmation handleToggle={handleToggle} handleDelete={handleDeleteCourse} show={show} />
+            </div>
         </div>
     )
 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_172633) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_163314) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_172633) do
     t.bigint "instructor_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["instructor_id"], name: "index_courses_on_instructor_id"
   end
 
