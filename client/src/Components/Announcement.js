24a32bd10469bb82d2fa4edbe422523499055cb5
Nico Garbaccio <garbaccio20@gmail.com>
2022-11-30 14:46:50 -0500

    Assignments

--- client/src/Components/Announcement.js
+++ client/src/Components/Announcement.js
@@ -1,9 +1,100 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { UserContext } from '../Context/user';
+import parse from 'html-react-parser';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 function Announcement() {
+
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [announcement, setAnnouncement] = useState([])
+    const [show, setShow] = useState(false)
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const { user } = useContext(UserContext);
+    const [formData, setFormData] = useState([]);
+
+    useEffect(() => {
+        fetch(`/announcements/${id}`)
+        .then((r) => r.json())
+        .then(announcement => {
+        setAnnouncement(announcement);
+        setIsLoaded(true)
+        setFormData({
+            title: announcement.title,
+            body: announcement.body,
+            pinned: announcement.pinned
+        })
+    })
+    }, [id])
+
+    if (!isLoaded) return <h2>Loading...</h2>
+
+    function handleChange(e){
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    function toggleEdit(){
+        setShow(!show)
+    }
+
+    function handleDeleteAnnouncement(){
+        fetch(`/announcements/${id}`, {
+            method:'DELETE'
+          })
+        navigate(`/course/${announcement.course.id}/announcements`);
+    }
+
+    function handlePatch(e) {
+        e.preventDefault()
+        fetch(`/announcements/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+        })
+        .then(res => res.json())
+        .then(updatedAnnouncement => setAnnouncement(updatedAnnouncement))
+        setShow(!show)
+    }
+
     return (
-        <div>
-            Announcement
+        <div className='min-h-screen bg-slate-200 text-justify p-10'>
+            <div>
+                <h1 className='text-2xl font-bold text-center mb-5'>{announcement.title}</h1>
+                <p className='my-3'>{announcement.created_at.slice(0, 10)}</p>
+                <p>{parse(announcement.body)}</p>
+            </div>
+
+            {user?.admin ?
+                <>
+                <button onClick={toggleEdit} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-3 py-1 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-6 block">Edit Announcement</button>
+                <button onClick={handleDeleteAnnouncement} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-3 py-1 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-6 block">Delete Announcement</button>
+    
+                <form onSubmit={handlePatch} className={show ? "show w-1/2 mt-4" : "hide"}>
+
+                    <input type="text" id="title" placeholder="Title" name="title" value={formData.title} onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"></input>
+
+                    <CKEditor 
+                        editor={ClassicEditor}
+                        data={formData.body}
+                        onChange={(event, editor) => {
+                            const data = editor.getData()
+                            setFormData({ ...formData, [announcement.body]: data })
+                        }}
+                    /> 
+                    
+                    <button type='submit' className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-3 py-1 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 my-8">Submit</button>
+
+                </form>
+                </>
+            :
+                null
+            }
+
         </div>
     )
 }
--- client/src/Components/AnnouncementList.js
+++ client/src/Components/AnnouncementList.js
@@ -1,10 +1,94 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { useParams, NavLink, useNavigate } from 'react-router-dom';
+import { UserContext } from '../Context/user';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 function AnnouncementList() {
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [announcements, setAnnouncements] = useState([])
+    const { id } = useParams();
+    const { user } = useContext(UserContext);
+    const navigate = useNavigate();
+    const [title, setTitle] = useState("")
+    const [text, setText] = useState("")
+
+    useEffect(() => {
+        fetch(`/courses/${id}/announcements`)
+        .then((r) => r.json())
+        .then(announcements => {
+        setAnnouncements(announcements);
+        setIsLoaded(true)
+    })
+    }, [id])
+
+    if (!isLoaded) return <h2>Loading...</h2>
+
+    function handleChange(e){
+        setTitle(e.target.value)
+    };
+
+    function handleSubmit(e){
+        e.preventDefault();
+        fetch('/announcements', {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            },
+            body: JSON.stringify({
+                title: title,
+                body: text,
+                course_id: id
+              }),
+        })
+        .then((resp) => resp.json())
+        .then((announcement) => {
+            setAnnouncements([...announcements, announcement])
+            navigate(`/announcement/${announcement.id}`);
+        });
+    };
+
     return (
-        <div>
-            AnnouncementList
-        </div>
+        <div className='min-h-screen bg-slate-200 p-7'>
+        <h1 className='text-4xl font-bold my-8'>Announcements</h1>
+            { announcements.length  ? 
+                announcements.map(announcement => {
+                    return (
+                        <p className='my-8 ml-4'>
+                            <NavLink to={`/announcement/${announcement.id}`} className="hover:text-blue-700">
+                                <span className='font-bold'>{announcement.title}</span>
+                            </NavLink>
+                            <span> - {announcement.created_at.slice(5, 10)}</span>
+                        </p>
+
+                    )
+                })
+            :
+                <p>There are no announcements yet!</p>
+            }
+        {user?.admin ?
+            <>
+                <h2 className='text-xl font-bold my-8'>Add an announcement</h2>
+                <form onSubmit={handleSubmit} className="w-1/2">
+
+                    <input type="text" id="title" placeholder="Title" name="title" value={title} onChange={handleChange} className="block py-2.5 px-1 w-full text-sm text-gray-900 bg-white border-1 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer mb-6"></input>
+
+                    <CKEditor
+                    editor={ClassicEditor}
+                    data={text}
+                    onChange={(event, editor) => {
+                        const data = editor.getData()
+                        setText(data)
+                    }}
+                    /> 
+                    <button type='submit' className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-3 py-1 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-6">Submit</button>
+                </form>
+            </>
+        :
+            null
+        }
+     </div>
     )
 }
 
