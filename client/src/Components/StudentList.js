492624e1c3cb7acb5ff2a72697e6e03beb915dab
Nico Garbaccio <garbaccio20@gmail.com>
2022-12-01 09:51:29 -0500

    Student list

--- client/src/Components/StudentList.js
+++ client/src/Components/StudentList.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams, NavLink } from 'react-router-dom'; 
 
 function StudentList() {
+
+    const [courseStudents, setCourseStudents] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+    const { id } = useParams();
+
+    useEffect(() => {
+        fetch(`/courses/${id}/students`)
+        .then((r) => r.json())
+        .then(courseStudents => {
+        setCourseStudents(courseStudents);
+        setIsLoaded(true)
+    })
+    }, [id])
+
+    if (!isLoaded) return <h2>Loading...</h2>
+
     return (
-        <div>
-            StudentList
+        <div className='min-h-screen bg-slate-200 p-7'>
+            <h2 className='text-3xl font-bold mb-9 mt-5'>Students</h2>
+            {courseStudents.map(courseStudent => {
+                return (
+                    <p className='my-3 ml-2'>
+                        <NavLink to={`/course/${id}/students/${courseStudent.id}`} className="hover:text-blue-700">
+                            <span className='font-semibold'>{courseStudent.course_student_info}</span>
+                        </NavLink>
+                        <span> - {courseStudent.course_student_email}</span>
+                    </p>
+                )
+            })}
         </div>
     )
 }
